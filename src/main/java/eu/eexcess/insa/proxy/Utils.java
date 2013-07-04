0e3cb21491b9e2dcbfb65e058dbaf27ec0d62e2c
gaetan <gaetan@eexcess-stagiaire.insa-lyon.fr>
2013-07-04 14:26:14 +0200

    Fixed some bugs on the econbiz recommendation route

--- src/main/java/eu/eexcess/insa/proxy/Utils.java
+++ src/main/java/eu/eexcess/insa/proxy/Utils.java
@@ -11,18 +11,21 @@ import org.codehaus.jackson.JsonGenerator;
 public class Utils {
 	public static JsonFactory jsonFactory = new JsonFactory();
 	
-	public static void writeWeightedQuery(StringWriter request, HashMap<String,Double> query) throws IOException {
+	public static void writeWeightedQuery(StringWriter request, HashMap<String,Integer> query) throws IOException {
 		JsonGenerator jg = jsonFactory.createJsonGenerator(request);
 		jg = jsonFactory.createJsonGenerator(request);
 		jg.writeStartObject();
-			for(Entry<String, Double> entry: query.entrySet()) {
-				jg.writeArrayFieldStart("query");
+		jg.writeArrayFieldStart("query");
+			for(Entry<String, Integer> entry: query.entrySet()) {
+				//jg.writeArrayFieldStart("query");
+				//jg.writeFieldName("query");
 					jg.writeStartObject();
 						jg.writeStringField("term", entry.getKey());
 						jg.writeNumberField("score", entry.getValue());
 					jg.writeEndObject();
-				jg.writeEndArray();
+				//jg.writeEndArray();
 			}
+			jg.writeEndArray();
 		jg.writeEndObject();
 		jg.close();
 	}
