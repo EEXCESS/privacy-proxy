ea1a614305e5c6fa7ce0bc2a77515f4432fc497a
gperrin <gaetan.perrin@insa-lyon.fr>
2013-07-11 14:07:03 +0200

    added js/css to settings

---
+++ src/chrome/settings.js
@@ -0,0 +1,42 @@
+function doSwitchEmail() {
+	if($(".switch-email").attr("class") == "switch-animate switch-email switch-off"){
+		$(".switch-email").removeClass("switch-off");
+		$(".switch-email").addClass("switch-on");
+	}
+	else{
+		$(".switch-email").removeClass("switch-on");
+		$(".switch-email").addClass("switch-off");
+	}
+}
+
+function doSwitchGender() {
+	if($(".switch-gender").attr("class") == "switch-animate switch-gender switch-off"){
+		$(".switch-gender").removeClass("switch-off");
+		$(".switch-gender").addClass("switch-on");
+		$("#titleSetting").show();
+	}
+	else{
+		$(".switch-gender").removeClass("switch-on");
+		$(".switch-gender").addClass("switch-off");
+		$("#titleSetting").hide();
+		$("#titlePrivacy").attr("checked","false");
+	}
+}
+
+function doSwitchTitle() {
+	if($(".switch-title").attr("class") == "switch-animate switch-title switch-off"){
+		$(".switch-title").removeClass("switch-off");
+		$(".switch-title").addClass("switch-on");
+	}
+	else{
+		$(".switch-title").removeClass("switch-on");
+		$(".switch-title").addClass("switch-off");
+	}
+}
+
+
+$(document).ready(function(){
+	$('.emailSwitch').live("click",doSwitchEmail);
+	$('.genderSwitch').live("click",doSwitchGender);
+	$('.titleSwitch').live("click",doSwitchTitle);
+});
